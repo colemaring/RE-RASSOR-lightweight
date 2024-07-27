dd93d8ee725aff2a39d7a1b62443723fd8d8eaa6
Cole Maring <65455664+colemaring@users.noreply.github.com>
2024-07-27 18:17:10 -0400

    Add files via upload

---
+++ server/server.js
@@ -0,0 +1,114 @@
+const WebSocket = require("ws");
+
+// websocket server
+const wss = new WebSocket.Server({ port: 80 });
+//try 443?
+
+let connectedClients = [];
+
+// When a connection to a a client is established
+wss.on("connection", (ws, req) => {
+  const query = req.url.split("?")[1];
+  const params = new URLSearchParams(query);
+  const name = params.get("name");
+  console.log(`Client connected: ${name}`);
+
+  // Store the client name
+  ws.clientName = name;
+
+  // null name is broswer client, which shouldnt be added to list
+  if (name != null || name == "") {
+    connectedClients.push(name);
+  }
+
+  // Broadcast connected clients to all connected clients
+  wss.clients.forEach((client) => {
+    if (client !== ws && client.readyState === WebSocket.OPEN) {
+      client.send(
+        JSON.stringify({ type: "connectedClients", clients: connectedClients })
+      );
+    }
+  });
+
+  // getConnectedClients asks for the list of connected clients, used when client opens connection
+  ws.on("message", (message) => {
+    const data = JSON.parse(message);
+    if (data.type === "getConnectedClients") {
+      // Send the current list of connected rovers to the client
+      ws.send(
+        JSON.stringify({ type: "connectedClients", clients: connectedClients })
+      );
+    }
+    if (data.type === "move") {
+      // Relay the move message to the connected client
+      wss.clients.forEach((client) => {
+        if (
+          client !== ws &&
+          client.readyState === WebSocket.OPEN &&
+          client.clientName === ws.clientName
+        ) {
+          client.send(
+            JSON.stringify({ type: "move", direction: data.direction })
+          );
+        }
+      });
+    }
+  });
+
+  // send move message to appropriate rover client
+  ws.on("message", (message) => {
+    const data = JSON.parse(message);
+    const roverName = data.rover;
+    if (data.type === "move") {
+      // Relay the move message to the connected client where clientName matches roverName
+      wss.clients.forEach((client) => {
+        if (client.clientName === roverName) {
+          client.send(
+            JSON.stringify({ type: "move", direction: data.direction })
+          );
+        }
+      });
+    }
+  });
+
+  // Handle client disconnection
+  ws.on("close", () => {
+    // Remove client name from the array
+    connectedClients = connectedClients.filter((client) => client !== name);
+    console.log(`Connected clients: ${connectedClients}`);
+
+    // Broadcast connected clients to all connected clients
+    wss.clients.forEach((client) => {
+      if (client !== ws && client.readyState === WebSocket.OPEN) {
+        client.send(
+          JSON.stringify({
+            type: "connectedClients",
+            clients: connectedClients,
+          })
+        );
+      }
+    });
+  });
+
+  // Handle client errors
+  ws.on("error", (error) => {
+    console.error("Client error:", error);
+    // Remove client name from the array
+    connectedClients = connectedClients.filter((client) => client !== name);
+    console.log(`Connected clients: ${connectedClients}`);
+
+    // Broadcast connected clients to all connected clients
+    wss.clients.forEach((client) => {
+      if (client !== ws && client.readyState === WebSocket.OPEN) {
+        client.send(
+          JSON.stringify({
+            type: "connectedClients",
+            clients: connectedClients,
+          })
+        );
+      }
+    });
+  });
+});
+
+console.log("WebSocket server listening on port 80");
