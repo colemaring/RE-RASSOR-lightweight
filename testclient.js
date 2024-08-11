ec24e9434cbbbd3110258b7b8d5e177d34ac4c92
Cole Maring <65455664+colemaring@users.noreply.github.com>
2024-08-11 11:41:24 -0400

    Update testclient.js

--- testclient.js
+++ testclient.js
@@ -1,6 +1,6 @@
 const WebSocket = require("ws");
 
-const ws = new WebSocket("ws://localhost:80?name=superman");
+const ws = new WebSocket("ws://161.35.13.104:8080?name=superman");
 
 ws.on("open", () => {
   console.log("Connected to the WebSocket server");
