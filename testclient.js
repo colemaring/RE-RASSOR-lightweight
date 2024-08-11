c5c1c115c170cf6481680c149bf96a1973af40da
Cole Maring <65455664+colemaring@users.noreply.github.com>
2024-08-11 11:40:28 -0400

    Update and rename roverclient.js to testclient.js

--- testclient.js
+++ testclient.js
@@ -1,6 +1,3 @@
-// this will eventuall be a c++ file serving as a ws client on an ESP32
-// for now just testing with JS 
-
 const WebSocket = require("ws");
 
 const ws = new WebSocket("ws://localhost:80?name=superman");
