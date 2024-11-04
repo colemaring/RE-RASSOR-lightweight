43319ab79c599fd91ac4f9897882fcde522fb3ef
Cole Maring <65455664+colemaring@users.noreply.github.com>
2024-11-04 10:13:47 -0500

    Create testclient.js

---
+++ tests/testclient.js
@@ -0,0 +1,15 @@
+// for testing server and frontend development
+// run with node testclient.js
+
+const WebSocket = require("ws");
+
+const ws = new WebSocket("wss://rerassor.com?name=superman");
+
+ws.on("open", () => {
+  console.log("Connected to the WebSocket server");
+  ws.send(JSON.stringify({ type: "getConnectedClients" }));
+});
+
+ws.on("message", (message) => {
+  console.log(`Received message from server: ${message}`);
+});
