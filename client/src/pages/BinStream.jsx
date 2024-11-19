cad8f085e15426e54d4f8fba2f7e99a14bfa4e45
Cole Maring <65455664+colemaring@users.noreply.github.com>
2024-11-19 10:09:04 -0500

    Add files via upload

--- client/src/pages/BinStream.jsx
+++ client/src/pages/BinStream.jsx
@@ -1,17 +1,40 @@
 import React from "react";
 import MyNavbar from "../components/MyNavbar";
-import binstreamplaceholder from "../assets/binstreamplaceholder.png";
+import VideoJS from "../components/VideoJS";
 
 const BinStream = () => {
+  const playerRef = React.useRef(null);
+
+  const videoJsOptions = {
+    autoplay: true,
+    controls: true,
+    responsive: true,
+    fluid: true,
+    sources: [
+      {
+        src: "https://rerassor.com:8084/stream/test/channel/0/hls/live/index.m3u8",
+      },
+    ],
+  };
+
+  const handlePlayerReady = (player) => {
+    playerRef.current = player;
+
+    // You can handle player events here, for example:
+    player.on("waiting", () => {
+      videojs.log("player is waiting");
+    });
+
+    player.on("dispose", () => {
+      videojs.log("player will dispose");
+    });
+  };
+
   return (
     <>
       <MyNavbar />
-      <div className="streamContainer">
-        <img
-          src={binstreamplaceholder}
-          className="streamImage"
-          alt="Bin Stream"
-        />
+      <div className="videoStreamContainer">
+        <VideoJS options={videoJsOptions} onReady={handlePlayerReady} />
       </div>
     </>
   );
